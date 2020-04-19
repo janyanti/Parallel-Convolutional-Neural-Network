7967290b4de17f341d98ffe26e6d8f4fbc0f6580
Edward Lucero <elucero@ghc52.ghc.andrew.cmu.edu>
2020-04-18 21:27:46 -0400

    added more code

--- dcnn.cpp
+++ dcnn.cpp
@@ -1,19 +1,22 @@
 class sample_t {
   private:
-    float **x;
-    int **y;
+    float *x;
+    int *y;
 }
+
+enum layer_type {SIGM, SOFT, TANH, RELU, CONV}
 
 class model {
   private:
     int num_layers;
     int *num_units; 
+    layer_type *layer_types;
     // each layer has a 2d set of weights
-    float ***weights;
+    // float ***weights;
     float learning_rate;
     int num_epochs;
 
-    int num_samples;
+    
     int input_rows; 
     int input_cols; // should be 1 
     int output_rows; 
@@ -21,18 +24,35 @@ class model {
     sample_t *samples;
     
   public: 
-    void train();
-}
+    void train(sample_t *samples, int num_samples, int input_rows, int input_cols, int output_rows, int output_cols);
+    float** linearForward(float** v, float **w);
+    float** linearBackward();
+    float** sigmForward(float** v);
+    float** sigmBackward();
+    float** tanhForward(float** v);
+    float** tanhBackward();
+    float** reluForward(float** v);
+    float** reluBackward();
+    float** softForward(float** v);
+    float** softBackward(float** v);
+    float crossEntropyForward(float** v, float** vh);
+    float crossEntropyBackward();
+}     
 
 
-void model::train() {
+void model::train(sample_t *samples, int num_samples, int input_rows, 
+                  int input_cols, int output_rows, int output_cols) 
+{
   // iterators 
-  int w, e, s, i;
+  int e, s, i;
   // temp calculations 
-  float **temp[num_layers];
-  float **final[num_layers];
-  
+  float ***linearComp;
+  float ***activationComp;
+  float ***gradLinear;
+  float ***gradActivation;
+  float ***gradWeights; 
   // alloc weights 
+  float ***weights;
   int rows, cols;
   for (i = 0; i < num_layers; i++) {
     int rows = num_units[i];
@@ -56,14 +76,35 @@ void model::train() {
       sample_t s = samples[s];
       // forward computation
       for (i = 0; i < num_layers; i++) {
-        temp[i] = weights[i] * sample.x;
-        final[i] = append(ones,temp[i]);
+        if (layer_types[i] != CONV)
+        {
+          temp[i] = linearForward(sample.x, weights[i]);
+          if (layer_types[i] == SIGM)
+            final[i] = sigmForward(temp[i]);
+          else if (layer_types[i] == SOFT)
+            final[i] = softForward(temp[i]);
+        }
       }
+      float J = crossEntropyForward(samples.y, final[num_layers-1]);
+      float gJ = 1.;
+      gradActivation[num_layers - 1] = crossEntropyBackward();
       // backward computation
-      for (i = num_layers - 1; i > 0; i--) {
-        
+      for (i = num_layers - 1; i > 1; i--) {
+        if (layer_types[i] == SIGM) { 
+          gradLinear[i] = sigmBackward();
+          
+        } else if (layer_types[i] == SOFT) {
+          gradLinear[i] = softBackward();
+        }
+        gradWeights[i], gradActivation[i-1] = linearBackward();
       }
+
+      gradWeights[0], gx = linearBackward();
+
       // update all the weights
+      for (i = 0; i < num_layers; i++) {
+        weights[i] -= learning_rate * gradWeights[i];
+      }
     }
   }
 }
